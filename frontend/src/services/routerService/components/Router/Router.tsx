6e5dd49b2ef5079876fea1ceaef27be11f9ab549
Iskander Karimov <proenix575@gmail.com>
2022-05-07 15:22:08 +0300

    Add layout

--- frontend/src/services/routerService/components/Router/Router.tsx
+++ frontend/src/services/routerService/components/Router/Router.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { SignUpPageContainer } from "../../../signUpService/signUpService.container";
+import { Layout } from "./components/Layout/Layout";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SignUpPageContainer />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<SignUpPageContainer />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
