0249d362596d6b5f1df8dc53079a8940bcdee292
Iskander Karimov <proenix575@gmail.com>
2022-05-09 16:19:27 +0300

    Update

--- server/src/main.ts
+++ server/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: ['localhost:3000'],
+    origin: ['http://localhost:3000'],
   });
 
   await app.listen(9000, () => console.log(`server has been started`));
